eeb79559eb95862d009c3575a77b66c56fdf3f9a
singhshivam <shivam@blogvault.net>
2017-12-22 12:46:18 +0530

    making events flexible to accept any measurement protocol parameter

--- lib/gaevents/event.rb
+++ lib/gaevents/event.rb
@@ -1,31 +1,22 @@
 class GAEvents
 	class Event
-		attr_accessor :client, :category, :action, :label, :value
+		attr_accessor :params
 
-		def initialize(client, category, action, label = nil, value = nil)
-			@client = client
-			@category = category
-			@action = action
-			@label = label
-			@value = value
-		end
+		# Initialize Events by passing a hash.
+		# Keys could be any GA allowed parameter.
+		# Please refer Measurement Protocol Parameter Reference for available options:
+		# https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
 
-		def params(tid)
-			params = {
-			"v" => 1,
-			"tid" => tid,
-			"cid" => @client,
-			"t" => "event",
-			"ec" => @category,
-			"ea" => @action,
-			"el" => @label,
-			"ev" => @value
-			}
-			params.reject! { |k,v| !v }
+		# As per Measurement Protocol, parameters: v, tid, cid and t should always be present.
+		# This gem automatically injects v and tid. Ensure you always pass cid and t when
+		# initializing events.
+		def initialize(hash = {})
+			@params = hash.reject { |k,v| !v }
+			@params["v"] = 1
 		end
 
 		def payload(tid)
-			URI.encode_www_form params(tid)
+			URI.encode_www_form(@params.merge({"tid": tid}))
 		end
 	end
 end
